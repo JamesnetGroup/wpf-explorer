16621bf5f71961726521a46ed01f84fbf3e559ba
JAEWUNG LEE <james@jamesnet.dev>
2023-06-04 23:58:29 +0900

    Updates

---
+++ sample/WpfExplorer/WpfExplorer/App.cs
@@ -0,0 +1,18 @@
+﻿using Jamesnet.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfExplorer
+{
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new Window();
+        }
+    }
+}
